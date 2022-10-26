b6da402be053d497de6dbee008150b9271fe78bf
Andy Elliott <15898957+andye2004@users.noreply.github.com>
2022-10-26 14:43:19 +0800

    Initial commit

---
+++ src/main/java/com/example/nobeansdemo/Dao.java
@@ -0,0 +1,2 @@
+package com.example.nobeansdemo;public class Dao {
+}
---
+++ src/main/java/com/example/nobeansdemo/NoBeansDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.nobeansdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoBeansDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoBeansDemoApplication.class, args);
+	}
+
+}

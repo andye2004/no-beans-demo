63cec52361ec539adf2bd20e9d06f15306328278
Andy Elliott <15898957+andye2004@users.noreply.github.com>
2022-10-26 15:06:58 +0800

    Use @SpringBootApplication

--- src/main/java/com/example/nobeansdemo/NoBeansDemoApplication.java
+++ src/main/java/com/example/nobeansdemo/NoBeansDemoApplication.java
@@ -1,11 +1,9 @@
 package com.example.nobeansdemo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 public class NoBeansDemoApplication {
 
     public static void main(String[] args) {
